ebe271cf2b2fa67dfa13b54d3630b181aa253a9f
MD Najrul Islam Sumon <nazrul171992@gmail.com>
2025-02-09 19:55:25 +0600

    added-user

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
-
-}
+export class UserComponent {}
