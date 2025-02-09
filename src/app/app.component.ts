1c7a96014c8bbc04452eb28ed432378f4ba638bf
MD Najrul Islam Sumon <nazrul171992@gmail.com>
2025-02-09 20:00:15 +0600

    edited-html-of-app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { UserComponent } from './components/user/user.component';
+import { UserComponent } from "./components/user/user.component";
 
 @Component({
   selector: 'app-root',
