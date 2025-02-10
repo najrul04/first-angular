1b927263f02561be671300cf378e28d2dbb10982
MD Najrul Islam Sumon <nazrul171992@gmail.com>
2025-02-11 00:05:44 +0600

    added-count

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -11,12 +11,20 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 })
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
+  number = 0;
 
   get imagePath() { 
     return 'assets/users/' + this.selectedUser.avatar;
   }
 
   onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    this.selectedUser = DUMMY_USERS[randomIndex];
     console.log("Clicked")
+  }
+
+  onCount(){
+     this.number = this.number + 1;
+    console.log("Number Button clicked")
   }
 }
