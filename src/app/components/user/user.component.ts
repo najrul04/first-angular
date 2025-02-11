4f6def691893dbe99861969c29d2e42cd9bafb56
MD Najrul Islam Sumon <nazrul171992@gmail.com>
2025-02-12 00:16:48 +0600

    used-signals

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { DUMMY_USERS } from '../../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
@@ -10,16 +10,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]) 
+imagePath = computed(() => '../../../assets/users/' + this.selectedUser().avatar);
   number = 0;
 
-  get imagePath() { 
+/*   get imagePath() { 
     return 'assets/users/' + this.selectedUser.avatar;
-  }
+  } */
 
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]) 
     console.log("Clicked")
   }
 
